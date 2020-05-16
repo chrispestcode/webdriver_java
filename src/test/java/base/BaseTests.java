ce8b0256055531206fb89fefeda214d1a3228dc1
Tyree Chrisp <chri.tyree@gmail.com>
2020-05-16 16:14:33 -0700

    added web element selection

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,7 +1,11 @@
 package base;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class BaseTests {
 
@@ -11,6 +15,14 @@ public class BaseTests {
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
+
+        List<WebElement> links = driver.findElements(By.tagName("a"));
+        System.out.println(links.size());
+
+        WebElement inputsLink = driver.findElement(By.linkText("Inputs"));
+        inputsLink.click();
+
+
         System.out.println(driver.getTitle());
         driver.quit();
     }
