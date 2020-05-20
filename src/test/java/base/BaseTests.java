1d0efdbb4396ee4dd567b254fca1e8198e9685d9
Tyree Chrisp <chri.tyree@gmail.com>
2020-05-20 05:09:01 -0700

    added screenshot functionality

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,10 +1,11 @@
 package base;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import com.google.common.io.Files;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
@@ -12,6 +13,8 @@ import pages.LoginPage;
 import pages.SecureAreaPage;
 import utils.WindowManager;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 public class BaseTests {
@@ -35,6 +38,21 @@ public class BaseTests {
     @BeforeMethod
     public void goHome(){
         driver.get("https://the-internet.herokuapp.com/");
+    }
+
+    @AfterMethod
+    public void recordFailure(ITestResult result) {
+        if(ITestResult.FAILURE == result.getStatus()) {
+            var camera = (TakesScreenshot) driver;
+            File screenshot = camera.getScreenshotAs(OutputType.FILE);
+            System.out.println("Screenshot taken: " + screenshot.getAbsolutePath());
+
+            try {
+                Files.move(screenshot, new File("resources/screenshots/" + result.getName() + ".png"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @AfterClass
